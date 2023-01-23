481f89a912a57a4ec20ccdc6b68e6087dfafb2c6
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-23 21:06:15 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,41 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - function that sorts a doubly linked list
+ *                       of integers in ascending order using the Insertion
+ *                       sort algorithm
  * @list: doubly linked list
- *
- * Return: Nothing
+ * Return: void
  */
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current = *list;
-    listint_t *key;
-    listint_t *j;
+	listint_t *node = NULL, *tmp = NULL;
 
-    for (current = (*list)->next; current != NULL; current = current->next)
-    {
-        key = current;
-        j = current->prev;
-        while (j != NULL && j->n > key->n)
-        {
-            j->next->prev = j->prev;
-            if (j->prev != NULL)
-                j->prev->next = j->next;
-            else
-                *list = j->next;
-            j->prev = key->prev;
-            j->next = key;
-            key->prev->next = j;
-            key->prev = j;
-            j = j->prev;
-            print_list(*list);
-        }
-    }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	node = *list;
+	node = node->next;
+	while (node)
+	{
+		while (node->prev && node->n < (node->prev)->n)
+		{
+			tmp = node;
+			if (node->next)
+				(node->next)->prev = tmp->prev;
+			(node->prev)->next = tmp->next;
+			node = node->prev;
+			tmp->prev = node->prev;
+			tmp->next = node;
+			if (node->prev)
+				(node->prev)->next = tmp;
+			node->prev = tmp;
+			if (tmp->prev == NULL)
+				*list = tmp;
+			print_list(*list);
+			node = node->prev;
+		}
+		node = node->next;
+	}
+
 }
-

86bec020e3f9774dfef1cd98b6fa4cecbcbd6cfd
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-23 21:15:26 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,39 +1,24 @@
+
 #include "sort.h"
 
 /**
- * quick_sort - function that sorts an array of integers
- *              in ascending order using the Quick sort algorithm
- * @array: array
- * @size: array's size
- * Return: void
+ * partition - array partition
+ * @array: array to sort
+ * @first: first position
+ * @last: last position
+ * @size: array size
+ * Return: int pivot index
  */
-void quick_sort(int *array, size_t size)
+int partition(int *array, int first, int last, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+	int i = first - 1, aux, j;
 
-	quick_s(array, 0, size - 1, size);
-}
-
-/**
- * partition - partition
- * @array: array
- * @lo: lower
- * @hi: higher
- * @size: array's size
- * Return: i
- */
-int partition(int *array, int lo, int hi, size_t size)
-{
-	int i = lo - 1, j = lo;
-	int pivot = array[hi], aux = 0;
-
-	for (; j < hi; j++)
+	for (j = first; j <= last - 1; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] < array[last])
 		{
 			i++;
-			if (array[i] != array[j])
+			if (i < j)
 			{
 				aux = array[i];
 				array[i] = array[j];
@@ -42,32 +27,43 @@ int partition(int *array, int lo, int hi, size_t size)
 			}
 		}
 	}
-	if (array[i + 1] != array[hi])
+	if (array[i + 1] > array[last])
 	{
 		aux = array[i + 1];
-		array[i + 1] = array[hi];
-		array[hi] = aux;
+		array[i + 1] = array[last];
+		array[last] = aux;
 		print_array(array, size);
 	}
+
 	return (i + 1);
 }
 
 /**
- * quick_s - quick sort
- * @array: given array
- * @lo: lower
- * @hi:higher
- * @size: array's size
- * Return: void
+ * qs - sorts an array of integers recursively
+ * @array: array to sort
+ * @first: first position
+ * @last: last position
+ * @size: array size
  */
-void quick_s(int *array, int lo, int hi, size_t size)
+void qs(int *array, int first, int last, size_t size)
 {
 	int pivot;
 
-	if (lo < hi)
+	if (first < last)
 	{
-		pivot = partition(array, lo, hi, size);
-		quick_s(array, lo, pivot - 1, size);
-		quick_s(array, pivot + 1, hi, size);
+		pivot = partition(array, first, last, size);
+		qs(array, first, pivot - 1, size);
+		qs(array, pivot + 1, last, size);
 	}
+}
+
+/**
+ * quick_sort - sorts an array of integers using the Quick
+ * sort algorithm in ascending order
+ * @array: array to sort
+ * @size: array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	qs(array, 0, size - 1, size);
 }
